6e9a296fac4d3e87dede00a846b872bf4d763866
cluz123 <pmlizarraga@gmail.com>
2022-11-27 17:48:54 -0300

    server.js routes and functions missing added

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const fs = require("fs");
 const { default: axios } = require("axios");
 const { execFile } = require("child_process");
 const blk = require('linux-blockutils');
-const { resolveAny } = require("dns");
 
 
 
@@ -20,8 +19,8 @@ app.get('/showdrives', function (req, res, next) {
 });
 
 
-function checkFunction(sdx, type) {
-  execFile('bash', ['/home/revo/nodeutils', type, sdx], (err, stdout, stderr) => {
+function checkFunction(disk, type) {
+  execFile('bash', ['/home/revo/nodeutils', type, disk], (err, stdout, stderr) => {
     if (err) {
       return (err);
     } else {
@@ -32,29 +31,29 @@ function checkFunction(sdx, type) {
 
 app.post('/checkdrive', (req, res, next) => {
   const { disk1, disk2 } = req.body;
-  let res = [];
+  let response = [];
   if (!disk1 || !disk2) {
     res.status(404).send('You need at least 2 drives!');
   }
   resDisk1 = { disk1: checkFunction(disk1, '-checkdrive') };
-  res.push(resDisk1);
+  response.push(resDisk1);
   resDisk2 = { disk2: checkFunction(disk2, '-checkdrive') };
-  res.push(resDisk2);
-  res.send(res);
+  response.push(resDisk2);
+  res.send(response);
 
 });
 
 app.post('/checkfilesystem', (req, res, next) => {
   const { disk1, disk2 } = req.body;
-  let res = [];
+  let response = [];
   if (!disk1 || !disk2) {
     res.status(404).send('You need at least 2 drives!');
   }
   let resDisk1 = { disk1: checkFunction(disk1, '-checkfilesystem') };
-  res.push(resDisk1);
+  response.push(resDisk1);
   let resDisk2 = { disk2: checkFunction(disk2, '-checkfilesystem') };
-  res.push(resDisk2);
-  res.send(res);
+  response.push(resDisk2);
+  res.send(response);
 });
 
 app.post('/makearray', (req, res, next) => {
@@ -66,13 +65,94 @@ app.post('/makearray', (req, res, next) => {
       res.send(stdout);
     }
   });
-  
+
 });
 
+function getArrInfo(type) {
+  execFile('bash', ['/home/revo/nodeutils', type, 'md0'], (err, stdout, stderr) => {
+    if (err) {
+      return (err);
+    } else {
+      return (stdout);
+    }
+  });
+}
 
+app.get('/getarrayinfo', (req, res, next) => {
+  let arrDetails = getArrInfo('-arraydetails');
+  let arrStatus = getArrInfo('-arraystatus');
+  let arrUsage = getArrInfo('-arrayusage');
+  let response = {
+    arrayDetails: arrDetails,
+    arrayStatus: arrStatus,
+    arrayUsage: arrUsage
+  }
+  if (arrDetails && arrStatus && arrUsage) {
+    res.send(response)
+  } else {
+    res.status(404).send(response);
+  }
+})
 
+app.get('/wifiscan', (req, res, next) => {
+  execFile('bash', ['/home/revo/nodeutils', '-wifiscan'], (err, stdout, stderr) => {
+    if (err) {
+      res.status(404).send(err);
+    } else {
+      res.send(stdout);
+    }
+  });
+})
 
+app.post('/genwificonfig', (req, res, next) => {
+  const { essid, pass, country } = req.body;
+  execFile('bash', ['/home/revo/nodeutils', '-genwificonfig', essid, pass, country], (err, stdout, stderr) => {
+    if (err) {
+      res.status(404).send(err);
+    } else {
+      res.send(stdout);
+    }
+  });
+})
 
+app.post('/removearray', (req, res, next) => {
+  const { disk1, disk2 } = req.body;
+  execFile('bash', ['/home/revo/nodeutils', '-removearray', disk1, disk2, 'md0'], (err, stdout, stderr) => {
+    if (err) {
+      res.status(404).send(err);
+    } else {
+      res.send(stdout);
+    }
+  });
+})
+
+function wifiConfig(type) {
+  execFile('bash', ['/home/revo/nodeutils', type], (err, stdout, stderr) => {
+    if (err) {
+      return { err };
+    } else {
+      return { stdout };
+    }
+  });
+}
+
+app.get('/delwificonfig', (req, res, next) => {
+  let response = wifiConfig('-delwificonfig');
+  if(response.err){
+    res.status(404).send(response.err);
+  }else {
+    res.send(response.stdout);
+  }
+})
+
+app.get('/getwificonfig', (req, res, next) => {
+  let response = wifiConfig('-getwificonfig');
+  if(response.err){
+    res.status(404).send(response.err);
+  }else {
+    res.send(response.stdout);
+  }
+})
 
 const PORT = process.env.PORT || 3001;
 
