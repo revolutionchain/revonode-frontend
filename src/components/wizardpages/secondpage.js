0016a9e09c727cf8ada895ac426af51a5f2c222c
cluz123 <pmlizarraga@gmail.com>
2022-11-29 02:10:38 -0300

    code cleared

--- src/components/wizardpages/secondpage.js
+++ src/components/wizardpages/secondpage.js
@@ -1,72 +1,57 @@
 import React, { useEffect, useState } from 'react';
 
 
-export default function Secondpage({currentPage, setCurrentPage, drivesData}) {
+export default function Secondpage({ currentPage, setCurrentPage, drivesData }) {
 
-    const [ selectedDrives, setSelectedDrives ] = useState([]);
-    const [ updateStates, setUpdatesStates ] = useState(false);
-    const [ checkedState, setCheckedState ] = useState([]);
-    const [ totalDrives, setTotalDrives ] = useState([]);
+    const [selectedDrives, setSelectedDrives] = useState([]);
+    const [updateStates, setUpdatesStates] = useState(false);
+    const [checkedState, setCheckedState] = useState([]);
+    const [totalDrives, setTotalDrives] = useState([]);
     useEffect(() => {
-	let drives = drivesData.filter(e => e.NAME.includes("sd")).reverse();
+        let drives = drivesData.filter(e => e.NAME.includes("sd")).reverse();
         let ssdCount = drives.map(e => 0);
         setCheckedState(ssdCount);
-	setTotalDrives(drives);
+        setTotalDrives(drives);
     }, [])
 
     function handleCheckbox(elem, position) {
-        let newArr = checkedState.map((e,i) => {
-            if(((e == 1 || e == 2) && position == i) || (e == 0 && position !== i)){
+        let newArr = checkedState.map((e, i) => {
+            if (((e == 1 || e == 2) && position == i) || (e == 0 && position !== i)) {
                 return 0;
-            }else if(e == 1 && checkedState[position] !== 2 && i !== position){
+            } else if (e == 1 && checkedState[position] !== 2 && i !== position) {
                 return 2;
-            }else if(e == 1 && checkedState[position] == 2 && i !== position){
+            } else if (e == 1 && checkedState[position] == 2 && i !== position) {
                 return 1;
-            }else if(e == 2  && checkedState[position] == 1 && i !== position){
+            } else if (e == 2 && checkedState[position] == 1 && i !== position) {
                 return 1;
-            }else if(e !== 1 && i == position){
+            } else if (e !== 1 && i == position) {
                 return 1;
             }
         });
         setCheckedState(newArr);
-	let drives = [];
-	newArr.map((e,i) => {
-		if(e == 1){
-			drives[1] = totalDrives[i];
-		}else if(e == 2){
-			drives[0] = totalDrives[i];
-		}
-	})
-	if(!newArr.includes(2)){
-		drives[0] = null;
-	}
-	if(!newArr.includes(1)){
-		drives[1] = null;
-	}
-
-/*
-            if(!drives[1] && newArr[i] == 1){
-                drives[1] = elem;
-            }else if(drives[1] && newArr[i] == 1){
-                drives[0] = drives[1];
-                drives[1] = elem;
-            }else if(!newArr.includes(2) && newArr[i] == 1){
-                drives[1] = elem;
-                drives[0] = null;
+        let drives = [];
+        newArr.map((e, i) => {
+            if (e == 1) {
+                drives[1] = totalDrives[i];
+            } else if (e == 2) {
+                drives[0] = totalDrives[i];
             }
         })
-*/
-        
-
+        if (!newArr.includes(2)) {
+            drives[0] = null;
+        }
+        if (!newArr.includes(1)) {
+            drives[1] = null;
+        }
         setSelectedDrives(drives);
         updateStates ? setUpdatesStates(false) : setUpdatesStates(true);
     }
 
 
     function handleNextButton() {
-        if(checkedState.includes(1) && checkedState.includes(2)){
+        if (checkedState.includes(1) && checkedState.includes(2)) {
             setCurrentPage(currentPage + 1)
-        }else {
+        } else {
             alert('You must select at least 2 drives.');
         }
     }
@@ -77,18 +62,18 @@ export default function Secondpage({currentPage, setCurrentPage, drivesData}) {
             <h3> Description text!</h3>
             <div>
                 {
-                    drivesData.filter(e => e.NAME.includes("sd")).reverse().map((e,i) => {
+                    drivesData.filter(e => e.NAME.includes("sd")).reverse().map((e, i) => {
                         return <div key={e.NAME}>
-                            <input type="checkbox" checked={checkedState[i]} onClick={() => handleCheckbox(e,i)}></input>
+                            <input type="checkbox" checked={checkedState[i]} onClick={() => handleCheckbox(e, i)}></input>
                             <span>{e.NAME}</span>
-                            <span>{((parseFloat(e.SIZE))/1000000000).toFixed(2) + ' gb'}</span>
+                            <span>{((parseFloat(e.SIZE)) / 1000000000).toFixed(2) + ' gb'}</span>
                         </div>
                     })
                 }
             </div>
             <button onClick={() => setCurrentPage(currentPage - 1)} className='next-button'>Back</button>
             <button onClick={() => handleNextButton()} className='next-button'>Next</button>
-        
+
         </div>
     )
 }
