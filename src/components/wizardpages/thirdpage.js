a49c4cfb58e7d7caaed091769b04b08ba3888dec
cluz123 <pmlizarraga@gmail.com>
2022-11-29 16:13:33 -0300

    arrayData modified

--- src/components/wizardpages/thirdpage.js
+++ src/components/wizardpages/thirdpage.js
@@ -10,7 +10,7 @@ export default function Thirdpage({currentPage, setCurrentPage}) {
 
     useEffect( async () => {
         let getarrayinfo = await axios.get(`http://${REACT_APP_LOCAL_NODE_IP}:3001/getarrayinfo`);
-        setArrayData(getarrayinfo.data.arrayStatus.split(" "));
+        setArrayData(getarrayinfo.data.arrayStatus.split(" ").filter((e,i) => [3, 6, 7, 8, 14].includes(i)));
     },[])
 
     return (
