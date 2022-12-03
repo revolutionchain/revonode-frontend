cc7567ed08e704c260cfdc8423827ca2baa78b2f
cluz123 <pmlizarraga@gmail.com>
2022-12-03 00:24:02 -0300

    ip handler modified

--- src/components/wizardpages/secondpage.js
+++ src/components/wizardpages/secondpage.js
@@ -4,7 +4,7 @@ import Select from 'react-select';
 const { REACT_APP_LOCAL_NODE_ETH_IP } = process.env;
 const { REACT_APP_LOCAL_NODE_WIFI_IP } = process.env;
 
-const REACT_APP_LOCAL_NODE_IP = REACT_APP_LOCAL_NODE_WIFI_IP || REACT_APP_LOCAL_NODE_ETH_IP;
+const REACT_APP_LOCAL_NODE_IP = REACT_APP_LOCAL_NODE_WIFI_IP ? REACT_APP_LOCAL_NODE_WIFI_IP : REACT_APP_LOCAL_NODE_ETH_IP;
 
 
 export default function Secondpage({ currentPage, setCurrentPage, drivesData }) {
