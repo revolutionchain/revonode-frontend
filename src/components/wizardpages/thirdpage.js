88e55c913ce0e93e03539cffc1e2924f6c9bbe37
cluz123 <pmlizarraga@gmail.com>
2022-12-22 01:56:18 -0300

    arraydata fix

--- src/components/wizardpages/thirdpage.js
+++ src/components/wizardpages/thirdpage.js
@@ -17,8 +17,7 @@ export default function Thirdpage({ currentPage, setCurrentPage }) {
 
     useEffect(async () => {
         let getarrayinfo = await axios.get(`http://${REACT_APP_LOCAL_NODE_IP}:3001/getarrayinfo`);
-        console.log(getarrayinfo.data.arrayStatus);
-        setArrayData(getarrayinfo.data.arrayStatus.split("md0")[1].split(" ").filter((e, i) => [3, 6, 7, 8, 14].includes(i)));
+        setArrayData(["md0"].concat(getarrayinfo.data.arrayStatus.split("md0")[1].split(" ").filter((e, i) => [3, 4, 5, 11].includes(i))));
     }, [])
 
     const [modalIsOpen, setIsOpen] = React.useState(false);
