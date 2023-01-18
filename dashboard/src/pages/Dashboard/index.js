452def77d263c013acdb99fb0b3875dc2a02db80
cluz123 <pmlizarraga@gmail.com>
2023-01-18 16:27:00 -0300

    MapChart width modified

--- dashboard/src/pages/Dashboard/index.js
+++ dashboard/src/pages/Dashboard/index.js
@@ -115,7 +115,7 @@ const Dashboard = props => {
             <EarningReports nodeData={nodeData} peersData={peersData}  />
           </Row>
           <Row>
-            <Col xl={6}>
+            <Col xl={10}>
               {ipLocationData && <LatestTransactions ipLocationData={ipLocationData} />}
             </Col>{/*
             <Col xl={6}>
