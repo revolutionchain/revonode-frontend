0a9abcdf483ab59f9c6644b4ed21e2ab9b934594
cluz123 <pmlizarraga@gmail.com>
2023-01-23 14:00:20 -0300

    block time fix

--- wizard/server.js
+++ wizard/server.js
@@ -441,7 +441,8 @@ function globalDashboardFunction(type) {/*
     let getBlockCountResponse = execFileSync('bash', ['/home/revo/nodeutils', '-getblockcount'], { encoding: 'utf8' });
     let getBlockHash = execFileSync('bash', ['/home/revo/nodeutils', '-getblockhash', getBlockCountResponse], { encoding: 'utf8' });
     let getBlock = execFileSync('bash', ['/home/revo/nodeutils', '-getblock', getBlockHash], { encoding: 'utf8' });
-    return getBlock
+    result = Math.floor(Date.now()/1000) - getBlock;
+    return result;
   }
   try {
     return execFileSync('bash', ['/home/revo/nodeutils', type], { encoding: 'utf8' });
