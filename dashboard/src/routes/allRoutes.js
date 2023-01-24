a868c254c9668c14428a44a9cc121a81f69c9092
RevolutionChain Italy <62940168+revolutionchain@users.noreply.github.com>
2023-01-24 15:16:48 +0100

    Update allRoutes.js

--- dashboard/src/routes/allRoutes.js
+++ dashboard/src/routes/allRoutes.js
@@ -1,18 +1,9 @@
 import React from "react"
 import { Redirect } from "react-router-dom"
 
-// Pages Component
-import Chat from "../pages/Chat/Chat"
-
 // Profile
 import UserProfile from "../pages/Authentication/user-profile"
 
-// Pages Calendar
-import Calendar from "../pages/Calendar/index"
-
-//Email
-import EmailInbox from "../pages/Email/email-inbox"
-import EmailRead from "../pages/Email/email-read"
 
 // Authentication related pages
 import Login from "../pages/Authentication/Login"
@@ -38,47 +29,6 @@ import Transactions from "../pages/Transactions/index"
 
 // Wallet
 import Wallet from "../pages/Wallet/index"
-
-// Charts
-import ChartApex from "../pages/Charts/Apexcharts"
-
-// Maps
-import Maps from "../pages/Maps/Maps"
-
-//Icons
-import IconBoxicons from "../pages/Icons/Boxicons"
-import IconDripicons from "../pages/Icons/Dripicons"
-import IconMaterialdesign from "../pages/Icons/Materialdesign"
-import IconFontawesome from "../pages/Icons/Fontawesome"
-
-//Tables
-import BasicTables from "../pages/Tables/BasicTables"
-import DatatableTables from "../pages/Tables/DatatableTables"
-
-// Forms
-import FormElements from "../pages/Forms/FormElement"
-import FormAdvanced from "../pages/Forms/FormAdvanced"
-
-//Ui
-import UiElements from '../pages/UiElemets/UiElemets'
-
-//Pages
-import PagesStarter from "../pages/Utility/pages-starter"
-import Preloader from '../pages/Utility/Preloader';
-import Profile from "../pages/Utility/Profile/Profile";
-import Invoice from "../pages/Utility/Invoice";
-import Maintenance from "../pages/Utility/Maintenance"
-import PagesComingsoon from "../pages/Utility/Comingsoon"
-import Timeline from "../pages/Utility/TimeLine"
-import Pricing from "../pages/Utility/Pricing"
-import Error404 from "../pages/Utility/Error404"
-import Error500 from "../pages/Utility/Error500"
-
-//Adanace kit
-import SweetAlert from '../pages/AdvanaceKit/SweetAlert';
-import RangeSlider from "../pages/AdvanaceKit/RangeSlider"
-import Notification from "../pages/AdvanaceKit/Notifications";
-import Carousel from "../pages/AdvanaceKit/Carousel";
 
 const userRoutes = [
   { path: "/dashboard", component: Dashboard },
