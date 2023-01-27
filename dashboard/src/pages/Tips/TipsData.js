5a5aceb7ab42a644a0fa8e5a88b426e9276ea092
RevolutionChain Italy <62940168+revolutionchain@users.noreply.github.com>
2023-01-27 23:48:07 +0100

    Test content data

--- dashboard/src/pages/Tips/TipsData.js
+++ dashboard/src/pages/Tips/TipsData.js
@@ -19,33 +19,36 @@ const icons = []
                         <Col xl={12} >
                         <Card>
                             <CardBody>
-<h4 className="card-title mb-2">Most useful tips for your node</h4>
-<hr />
-                
-          <div class="table-responsive">
-            <table class="table mb-0 table">
-              <thead>
-                <tr>
-                  <th>Type</th>
-                  <th>Sender</th>
-                  <th>Receiver</th>
-                  <th>Time</th>
-                  <th>TX Id</th>
-                  <th>Amount</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <th style={{borderBottom: "none"}} scope="row">incoming/outgoing/staked</th>
-                  <td style={{borderBottom: "none"}}>RNDTope6F6eRRtLWMRz87J3zDiB5txcriT</td>
-                  <td style={{borderBottom: "none"}}>RNDTope6F6eRRtLWMRz87J3zDiB5txcriT</td>                  
-                  <td style={{borderBottom: "none"}}>2023-01-22 22:40:12 (0 m ago)</td>
-                  <td style={{borderBottom: "none"}}><a href="https://mainnet.revo.network/tx/3c02d74714c908c28faf37ec1137caf483b328ba1d5eba814a251acca4e4164b">3c02d74714c908c28faf37ec1137caf483b328ba1d5eba814a251acca4e4164b</a></td>
-                  <td style={{borderBottom: "none"}}>186.932 RVO</td>
-                </tr>
-              </tbody>
-            </table>
+        <h4 className="card-title mb-2">Most useful tips for your node</h4>
+        <hr />
+        <div class="col-12">
+          <div class="card">
+            <div class="card-body">
+              <h4 class="card-title">Description list alignment</h4>
+              <p class="card-title-desc">Align terms and descriptions horizontally by using our grid system’s predefined classes (or semantic mixins). For longer terms, you can optionally add a <code class="highlighter-rouge">.text-truncate</code> class to truncate the text with an ellipsis. </p>
+              <dl class="row mb-0">
+                <dt class="col-sm-3">Description lists</dt>
+                <dd class="col-sm-9">A description list is perfect for defining terms.</dd>
+                <dt class="col-sm-3">Euismod</dt>
+                <dd class="col-sm-9">Vestibulum id ligula porta felis euismod semper eget lacinia odio sem nec elit.</dd>
+                <dd class="col-sm-9 offset-sm-3">Donec id elit non mi porta gravida at eget metus.</dd>
+                <dt class="col-sm-3">Malesuada porta</dt>
+                <dd class="col-sm-9">Etiam porta sem malesuada magna mollis euismod.</dd>
+                <dt class="col-sm-3 text-truncate">Truncated term is truncated</dt>
+                <dd class="col-sm-9">Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</dd>
+                <dt class="col-sm-3">Nesting</dt>
+                <dd class="col-sm-9 mb-0">
+                  <dl class="row mb-0">
+                    <dt class="col-sm-4">Nested definition list</dt>
+                    <dd class="col-sm-8">Aenean posuere, tortor sed cursus feugiat, nunc augue blandit nunc.</dd>
+                  </dl>
+                </dd>
+              </dl>
+            </div>
           </div>
+        </div>        
+          
+
           </CardBody>
           </Card>
           </Col>
