3907e0f596abc7007689f004c4739cec0cf735bf
RevolutionChain Italy <62940168+revolutionchain@users.noreply.github.com>
2023-01-27 21:49:10 +0100

    Add widget farAway

--- dashboard/src/pages/Blocks/index.js
+++ dashboard/src/pages/Blocks/index.js
@@ -23,6 +23,16 @@ const Blocks = props => {
 
   const [nodeData, setNodeData] = useState(false);
 
+  function farAway(seconds) {
+    var numyears = (Math.floor(seconds / 31536000)) > 0 ? ((Math.floor(seconds / 31536000)) + ((Math.floor(seconds / 31536000)) > 1 ? " years, " : " year, ")) : "" ;
+      var numdays = Math.floor((seconds % 31536000) / 86400) > 0 ? (Math.floor((seconds % 31536000) / 86400) + (Math.floor((seconds % 31536000) / 86400) > 1 ? " days, " : " day, ")) : "" ;
+      var numhours = Math.floor(((seconds % 31536000) % 86400) / 3600) > 0 ? (Math.floor(((seconds % 31536000) % 86400) / 3600) + (Math.floor(((seconds % 31536000) % 86400) / 3600) > 1 ? " hours, " : " hour, ")) : "" ;
+      var numminutes = Math.floor((((seconds % 31536000) % 86400) % 3600) / 60) > 0 ? (Math.floor((((seconds % 31536000) % 86400) % 3600) / 60) + ( Math.floor((((seconds % 31536000) % 86400) % 3600) / 60) > 1 ? " minutes, " : " minute, ")) : "";
+      var numseconds = Math.floor((((seconds % 31536000) % 86400) % 3600) % 60);
+      let result =  numyears + numdays + numhours + numminutes + numseconds + " seconds"
+      return result;
+    }
+
   useEffect(() => {
     if (!isLogged) {
       props.history.push('/login');
@@ -67,7 +77,7 @@ const Blocks = props => {
             />
           }
           {/* import Widget */}
-          <Widget nodeData={nodeData} />
+          <Widget nodeData={nodeData} farAway={farAway} />
           <BlocksData />
 
         </Container>
