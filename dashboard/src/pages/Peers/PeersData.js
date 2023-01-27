55d0daf40f1d2596c423116f03d6e62201076c1d
RevolutionChain Italy <62940168+revolutionchain@users.noreply.github.com>
2023-01-27 11:31:03 +0100

    Temporary fix

--- dashboard/src/pages/Peers/PeersData.js
+++ dashboard/src/pages/Peers/PeersData.js
@@ -4,6 +4,16 @@ import { useEffect } from 'react';
 import Flag from 'react-world-flags'
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css"
 
+function secondsToString(seconds) {
+  var numyears = (Math.floor(seconds / 31536000)) > 0 ? ((Math.floor(seconds / 31536000)) + ((Math.floor(seconds / 31536000)) > 1 ? " years, " : " year, ")) : "" ;
+  var numdays = Math.floor((seconds % 31536000) / 86400) > 0 ? (Math.floor((seconds % 31536000) / 86400) + (Math.floor((seconds % 31536000) / 86400) > 1 ? " days, " : " day, ")) : "" ;
+  var numhours = Math.floor(((seconds % 31536000) % 86400) / 3600) > 0 ? (Math.floor(((seconds % 31536000) % 86400) / 3600) + (Math.floor(((seconds % 31536000) % 86400) / 3600) > 1 ? " hours, " : " hour, ")) : "" ;
+  var numminutes = Math.floor((((seconds % 31536000) % 86400) % 3600) / 60) > 0 ? (Math.floor((((seconds % 31536000) % 86400) % 3600) / 60) + ( Math.floor((((seconds % 31536000) % 86400) % 3600) / 60) > 1 ? " minutes, " : " minute, ")) : "";
+  var numseconds = Math.floor((((seconds % 31536000) % 86400) % 3600) % 60);
+  let result =  numyears + numdays + numhours + numminutes + numseconds + " seconds"
+  return result;
+
+}
 
 const PeersDataWidget = props => {
 
--- dashboard/src/pages/Peers/index.js
+++ dashboard/src/pages/Peers/index.js
@@ -16,18 +16,6 @@ import Widget from './Widget';
 import { useSelector } from 'react-redux';
 import PeersData from './PeersData';
 
-function secondsToString(seconds) {
-  var numyears = (Math.floor(seconds / 31536000)) > 0 ? ((Math.floor(seconds / 31536000)) + ((Math.floor(seconds / 31536000)) > 1 ? " years, " : " year, ")) : "" ;
-  var numdays = Math.floor((seconds % 31536000) / 86400) > 0 ? (Math.floor((seconds % 31536000) / 86400) + (Math.floor((seconds % 31536000) / 86400) > 1 ? " days, " : " day, ")) : "" ;
-  var numhours = Math.floor(((seconds % 31536000) % 86400) / 3600) > 0 ? (Math.floor(((seconds % 31536000) % 86400) / 3600) + (Math.floor(((seconds % 31536000) % 86400) / 3600) > 1 ? " hours, " : " hour, ")) : "" ;
-  var numminutes = Math.floor((((seconds % 31536000) % 86400) % 3600) / 60) > 0 ? (Math.floor((((seconds % 31536000) % 86400) % 3600) / 60) + ( Math.floor((((seconds % 31536000) % 86400) % 3600) / 60) > 1 ? " minutes, " : " minute, ")) : "";
-  var numseconds = Math.floor((((seconds % 31536000) % 86400) % 3600) % 60);
-  let result =  numyears + numdays + numhours + numminutes + numseconds + " seconds"
-  return result;
-
-}
-
-
 const Peers = props => {
 
   const isLogged = useSelector(state => state.Login.isLogged);
@@ -88,8 +76,8 @@ const Peers = props => {
             />
           }
           {/* import Widget */}
-          {ipLocationData && <Widget peersData={peersData} ipLocationData={ipLocationData} secondsToString={secondsToString} />}
-          {ipLocationData && <PeersData peersData={peersData} ipLocationData={ipLocationData} secondsToString={secondsToString} />}
+          {ipLocationData && <Widget peersData={peersData} ipLocationData={ipLocationData}  />}
+          {ipLocationData && <PeersData peersData={peersData} ipLocationData={ipLocationData} />}
 
         </Container>
       </div>
