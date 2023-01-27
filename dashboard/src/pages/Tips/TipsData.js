ebc2aaf991ce7fd91914c2b2aceab58c0728acfa
RevolutionChain Italy <62940168+revolutionchain@users.noreply.github.com>
2023-01-28 00:31:48 +0100

    Add wired vs wifi info

--- dashboard/src/pages/Tips/TipsData.js
+++ dashboard/src/pages/Tips/TipsData.js
@@ -23,7 +23,7 @@ const icons = []
                                     <div class="card">
                                       <div class="card-body">
                                         <h4 class="card-title">Backup your wallet!</h4>
-                                        <p class="card-title-desc">
+                                        <p>
                                         Backing up your Revo blockchain wallet is essential because it allows you to <b>restore your wallet</b> in case of any issues or hardware failures. <br></br>
                                         The backup file, named <b>wallet.dat</b>, contains all of the information needed to access your funds and transactions on the Revo network. Your file is encrypted with the wallet password you choosed during the node installation wizard. Without the password, you will not be able to access your funds.
                                         It's important to note that the only way to restore a wallet without the encryption password is by having the address private key. <br></br>
@@ -34,14 +34,21 @@ const icons = []
                                       <br></br>
                                       <div class="card-body">
                                         <h4 class="card-title">Become a Full Node!</h4>
-                                        <p class="card-title-desc">
+                                        <p>
                                         Having a <b>public IP</b> (ipv4) and opening <b>port 6969</b> on your router is crucial for optimizing your Revo blockchain node's performance. With a public IP, your node is able to connect to up to 1000 peers on the network, increasing the possibility of staking RVO coins more frequently and providing greater reliability. On the other hand, nodes that are behind a NAT or firewall, with no public IP and open port, will only be able to connect to a maximum of 8 nodes as a relay node: limiting their ability to participate in the network effectively and reduces the chances of staking new coins. <br></br>
                                         Opening port 6969 for your Revo node and having a public IP is always recommended. If you can't open your port manually, you can contact your Internet Service Provider (ISP) for assistance.
                                         </p>
                                         <p>Follow the link below for instructions or to check if your 6969 port is open:</p>
-                                        <a target="_blank" href="https://www.yougetsignal.com/tools/open-ports/">Check if my port is open</a>
-                                        <a target="_blank" href="https://letmegooglethat.com/?q=what+is+my+public+ip%3F&l=1">What is my public IP?</a>
-                                        <a target="_blank" href="https://letmegooglethat.com/?q=how+to+open+ports+on+my+router">How to open ports on my router?</a>                 
+                                        <a target="_blank" href="https://www.yougetsignal.com/tools/open-ports/">Check if my port is open</a><br></br>
+                                        <a target="_blank" href="https://letmegooglethat.com/?q=what+is+my+public+ip%3F&l=1">What is my public IP?</a><br></br>
+                                        <a target="_blank" href="https://letmegooglethat.com/?q=how+to+open+ports+on+my+router">How to open ports on my router?</a><br></br>              
+                                      </div>
+                                      <br></br>
+                                      <div class="card-body">
+                                        <h4 class="card-title">Ethernet or WiFi?</h4>
+                                        <p >
+                                        A wired ethernet connection is generally considered to be more <b>reliable and faster</b> than a wireless connection such as WiFi. This is because wired connections are not subject to interference from other wireless devices or networks, and the signal is not subject to degradation over distance. Additionally, wired connections are typically faster than wireless connections, as they have a larger bandwidth and lower latency. This makes them more suitable for use in blockchain node server applications, where reliability and speed are important factors.
+                                        </p>                                        
                                       </div>
                                     </div>
                                   </div>      
