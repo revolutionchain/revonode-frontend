97057944ccd0e3b5680f4a964db720b0bd4d26dd
RevolutionChain Italy <62940168+revolutionchain@users.noreply.github.com>
2023-01-28 21:11:13 +0100

    Import Links

--- dashboard/src/pages/Tips/TipsData.js
+++ dashboard/src/pages/Tips/TipsData.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Card, CardBody, Col, Row } from 'reactstrap';
 import { useEffect, useState } from 'react';
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css"
-
+import { Link } from "react-router-dom"
 
 const TipsDataWidget = props => {
 
