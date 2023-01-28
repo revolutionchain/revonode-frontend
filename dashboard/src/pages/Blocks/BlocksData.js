3930bb62baf6fa44e0ef0308db4310cd413875b1
RevolutionChain Italy <62940168+revolutionchain@users.noreply.github.com>
2023-01-28 10:48:18 +0100

    Update timing

--- dashboard/src/pages/Blocks/BlocksData.js
+++ dashboard/src/pages/Blocks/BlocksData.js
@@ -38,7 +38,7 @@ useEffect(()=>{
                 <tr key={e.hash}>
                 <th style={{borderBottom: "none"}} scope="row">{e.height} </th>
                 <td style={{borderBottom: "none"}}><a href={"https://mainnet.revo.network/block/" + e.hash}>{e.hash} </a></td>
-                <td style={{borderBottom: "none"}}>{e.time} </td>
+                <td style={{borderBottom: "none"}}>{farAway(e.time)} ago</td>
                 <td style={{borderBottom: "none"}}>{e.size} </td>
                 <td style={{borderBottom: "none"}}>{(e.tx).length} </td>
               </tr>)
