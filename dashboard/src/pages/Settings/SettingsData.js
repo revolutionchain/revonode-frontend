c1a1aed312bd93364f83a04f2f9d38fa169096d8
RevolutionChain Italy <62940168+revolutionchain@users.noreply.github.com>
2023-01-31 14:49:14 +0100

    WiFi selector

--- dashboard/src/pages/Settings/SettingsData.js
+++ dashboard/src/pages/Settings/SettingsData.js
@@ -22,7 +22,7 @@ useEffect(()=>{
                         <p>Use this form to change your WiFi settings</p> 
                           <div class="card">                                     
                             <div class="card-body">
-                            
+                            <div class="react-switch-bg" style="height: 28px; width: 56px; margin: 0px; position: relative; background: rgb(2, 164, 153); border-radius: 14px; cursor: pointer; transition: background 0.25s ease 0s;"><div style="height: 28px; width: 30px; position: relative; opacity: 1; pointer-events: none; transition: opacity 0.25s ease 0s;"><div style="display: flex; justify-content: center; align-items: center; height: 100%; font-size: 12px; color: rgb(255, 255, 255); padding-right: 2px;"> Enable WiFi</div></div><div style="height: 28px; width: 30px; position: absolute; opacity: 0; right: 0px; top: 0px; pointer-events: none; transition: opacity 0.25s ease 0s;"><div style="display: flex; justify-content: center; align-items: center; height: 100%; font-size: 12px; color: rgb(255, 255, 255); padding-right: 2px;"> Disable WiFi</div></div></div>
                             
                           </div>
                         </div>
