33c50c04ffceede521ff16af02ab0f6d8c787fd0
RevolutionChain Italy <62940168+revolutionchain@users.noreply.github.com>
2023-01-31 13:39:32 +0100

    Update ProfileMenu.js

--- dashboard/src/components/CommonForBoth/TopbarDropdown/ProfileMenu.js
+++ dashboard/src/components/CommonForBoth/TopbarDropdown/ProfileMenu.js
@@ -58,10 +58,12 @@ const ProfileMenu = props => {
           <i className="mdi mdi-chevron-down d-none d-xl-inline-block"/>
         </DropdownToggle>
         <DropdownMenu className="dropdown-menu-end">
-          <DropdownItem tag="Link" to="/profile">
-            {" "}
-            <i className="bx bx-user font-size-16 align-middle me-1"/>
-            {props.t("Profile")}{" "}
+          <DropdownItem>
+            <Link to="/profile">
+              {" "}
+              <i className="bx bx-user font-size-16 align-middle me-1"/>
+              {props.t("Profile")}{" "}
+            </Link>
           </DropdownItem>
           <DropdownItem tag="a" href="/settings">            
             <i className="bx bx-wrench font-size-16 align-middle me-1"/>
