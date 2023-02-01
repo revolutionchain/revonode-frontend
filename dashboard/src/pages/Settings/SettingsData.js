4eb82b643e2e5a4ad1273df218ef97b9bccf5ac9
cluz123 <pmlizarraga@gmail.com>
2023-02-01 19:56:12 -0300

    button modal added

--- dashboard/src/pages/Settings/SettingsData.js
+++ dashboard/src/pages/Settings/SettingsData.js
@@ -4,6 +4,7 @@ import { useEffect } from 'react';
 import Flag from 'react-world-flags'
 import Select from 'react-select';
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css"
+import SweetAlert from "react-bootstrap-sweetalert"
 
 // availity-reactstrap-validation
 import { AvForm, AvField } from "availity-reactstrap-validation"
@@ -45,8 +46,45 @@ useEffect(()=>{
 
 })
 
+
+
+const [confirm_alert, setconfirm_alert] = useState(false)
+const [confirm_alert2, setconfirm_alert2] = useState(false)
+//const [success_msg, setsuccess_msg] = useState(false)
+const [success_dlg, setsuccess_dlg] = useState(false)
+const [dynamic_title, setdynamic_title] = useState("")
+const [dynamic_description, setdynamic_description] = useState("")
+const [error_dlg, seterror_dlg] = useState(false)
+
+
     return (
         <React.Fragment>
+          
+          {success_dlg ? (
+                        <SweetAlert
+                            success
+                            title={dynamic_title}
+                            onConfirm={() => {
+                                {/*setsuccess_dlg(false)*/}
+                                props.history.push('/login');
+                            }}
+                        >
+                            {dynamic_description}
+                        </SweetAlert>
+                    ) : null}
+
+                    {error_dlg ? (
+                        <SweetAlert
+                            error
+                            title={dynamic_title}
+                            onConfirm={() => {
+                                seterror_dlg(false)
+                            }}
+                        >
+                            {dynamic_description}
+                        </SweetAlert>
+                    ) : null}
+
             <Row>
                 <Col md={12} xl={12} className="">
                   <Col xl={12} >
@@ -208,10 +246,40 @@ useEffect(()=>{
                                                 options={options} />}
                                         </div>
                               </div>
-                              <div className="text-left mt-4">
+                              <div className="text-left mt-4">{/*
                                 <Button type="submit" color="success">
                                   Save/Modify
-                                </Button>
+                                              </Button>*/}
+                                              
+                        <Col xl={3} lg={4} sm={6} className="mb-2">
+                                    <div className="p-3">
+                                        <Button
+                                            color="primary"
+                                            onClick={() => {
+                                                setconfirm_alert(true)
+                                            }}
+                                            id="sa-success"
+                                        >
+                                            Save
+                    </Button>
+                                    </div>
+                                    {confirm_alert ? (
+                                        <SweetAlert
+                                            title="Are you sure?"
+                                            warning
+                                            showCancel
+                                            confirmButtonText="Yes, delete it!"
+                                            confirmBtnBsStyle="success"
+                                            cancelBtnBsStyle="danger"
+                                            onConfirm={() => {
+                                                
+                                            }}
+                                            onCancel={() => setconfirm_alert(false)}
+                                        >
+                                            Your dashboard Email will be changed!
+                                        </SweetAlert>
+                                    ) : null}
+                                </Col>
                               </div>
                             </AvForm>
                           </div>
