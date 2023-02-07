22c1a9aaff46d71839d69aa5e85ac57d016db4c9
cluz123 <pmlizarraga@gmail.com>
2023-02-06 22:53:54 -0300

    widget test

--- dashboard/src/pages/Dashboard/Widget.js
+++ dashboard/src/pages/Dashboard/Widget.js
@@ -81,61 +81,63 @@ const widget = [
 
 const Widget = props => {
 
-    const [ widgetState, setWidgetState ] = useState(false);
-
-    
-useEffect(()=>{        
-    let totalBytesSent = 0;
-    let totalBytesRecv = 0;
-    (props.peersData).map(e=> {
-        totalBytesSent = totalBytesSent + e.bytessent;
-        totalBytesRecv = totalBytesRecv + e.bytesrecv;
-    })
-
-    widget[0].count = props.nodeData[0].connections.total;
-    widget[1].count = props.nodeData[1].totalbytessent;
-    widget[1].text = (totalBytesSent/1000000000).toFixed(3) + " GB Upload of all peers";
-    widget[2].count = props.nodeData[1].totalbytesrecv;
-    widget[2].text = (totalBytesRecv/1000000000).toFixed(3) + " GB Download of all peers";
-    widget[3].count = props.nodeData[2].length > 0 ? props.nodeData[2].length : "0";
-    widget[4].count = props.nodeData[3].size;
-    widget[5].count = props.nodeData[0].blocks;
-    widget[5].text = props.farAway((props.nodeData[10].time)) + " ago";
-
-    setWidgetState(widget);
-},[props.nodeData])
+    const [widgetState, setWidgetState] = useState(false);
 
 
-return (
+    useEffect(() => {
+        let totalBytesSent = 0;
+        let totalBytesRecv = 0;
+        (props.peersData).map(e => {
+            totalBytesSent = totalBytesSent + e.bytessent;
+            totalBytesRecv = totalBytesRecv + e.bytesrecv;
+        })
+
+        widget[0].count = props.nodeData[0].connections.total;
+        widget[1].count = props.nodeData[1].totalbytessent;
+        widget[1].text = (totalBytesSent / 1000000000).toFixed(3) + " GB Upload of all peers";
+        widget[2].count = props.nodeData[1].totalbytesrecv;
+        widget[2].text = (totalBytesRecv / 1000000000).toFixed(3) + " GB Download of all peers";
+        widget[3].count = props.nodeData[2].length > 0 ? props.nodeData[2].length : "0";
+        widget[4].count = props.nodeData[3].size;
+        widget[5].count = props.nodeData[0].blocks;
+        widget[5].text = props.farAway((props.nodeData[10].time)) + " ago";
+
+        setWidgetState(widget);
+    }, [props.nodeData])
+
+
+    return (
         <React.Fragment>
             <Row>
-                
-                    <Col md={12} xl={12} className="d-flex flex-wrap">
-                    {widgetState && widgetState.map((widget, key) => (
-                        <Col xs={12}  sm={6} md={2} xl={2} key={key}>
-                        <Card>
-                            <CardBody>
-                                <div>
-                                    <p style={{fontSize: '14px'}} className="text-muted text-uppercase p-title"><i className={widget.icon}></i>{" " + widget.title}</p>
-                                </div>
-                                    <h4  style={{position: "absolute", top: "0", height: "100%", display: "flex", alignItems: "center"}} className="mb-1 mt-1">
-                                        <span className="counter-value" data-target="58425">
-                                            {widget.id == 2 || widget.id == 3 ? (widget.count/1073741824).toFixed(3) : widget.id == 6 ? <a target="_blank" href={"https://testnet.revo.network/block/" + widget.count}>{widget.count}</a> : widget.count }
-                                        </span></h4>
-                                <p style={{position: `absolute`, bottom: `0`}} className="text-muted mt-3 mb-2">
-                                    <span className={""}>
 
-                                        {/*widget.upArrow === true ?
+                <Col md={12} xl={12} className="d-flex flex-wrap">
+                    {widgetState && widgetState.map((widget, key) => (
+                        <Col xs={12} sm={6} md={2} xl={2} key={key}>
+                            <Card>
+                                <CardBody>
+                                    <div>
+                                        <p style={{ fontSize: '14px' }} className="text-muted text-uppercase p-title"><i className={widget.icon}></i>{" " + widget.title}</p>
+                                    </div>
+                                    <div style={{display: "flex", height: "100%", position: "absolute", top: "0", alignItems: "center"}}>
+                                        <h4  className="mb-1 mt-1">
+                                            <span className="counter-value" data-target="58425">
+                                                {widget.id == 2 || widget.id == 3 ? (widget.count / 1073741824).toFixed(3) : widget.id == 6 ? <a target="_blank" href={"https://testnet.revo.network/block/" + widget.count}>{widget.count}</a> : widget.count}
+                                            </span></h4>
+                                        <p style={{ position: `absolute`, bottom: `0` }} className="text-muted mt-3 mb-2">
+                                            <span className={""}>
+
+                                                {/*widget.upArrow === true ?
                                             <i className="mdi mdi-arrow-up-bold me-1"></i> : <i className="mdi mdi-arrow-down-bold me-1"></i>
                                         */}
-                                        <i className={widget.secondIcon}></i>{" " + widget.text}
-                                    </span> 
-                            </p>
-                            </CardBody>
-                        </Card>
+                                                <i className={widget.secondIcon}></i>{" " + widget.text}
+                                            </span>
+                                        </p>
+                                    </div>
+                                </CardBody>
+                            </Card>
                         </Col>
-                ))}
-                    </Col>
+                    ))}
+                </Col>
             </Row>
         </React.Fragment>
     );
