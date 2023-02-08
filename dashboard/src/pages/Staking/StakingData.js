8d8c29208e179fcd5d69fef2507c46626d0e4645
RevolutionChain Italy <62940168+revolutionchain@users.noreply.github.com>
2023-02-08 12:47:16 +0100

    Update target blank

--- dashboard/src/pages/Staking/StakingData.js
+++ dashboard/src/pages/Staking/StakingData.js
@@ -226,7 +226,7 @@ const StakingDataWidget = props => {
                         orderedState && (orderedState).map(e => {
                           return (
                             <tr key={e.txid} >
-                              <th style={{ borderBottom: "none", paddingLeft: "0" }} scope="row"><a className={e.confirmations < 500 ? "text-muted" : ""} href={`${props.nodeData[11].EXPLORER_URL}tx/${e.txid}`}>{e.txid} </a></th>
+                              <th style={{ borderBottom: "none", paddingLeft: "0" }} scope="row"><a target="_blank" className={e.confirmations < 500 ? "text-muted" : ""} href={`${props.nodeData[11].EXPLORER_URL}tx/${e.txid}`}>{e.txid} </a></th>
                               <td style={{ borderBottom: "none", paddingLeft: "0" }}><b>{e.amount + " RVO"}</b></td>
                               <td style={{ borderBottom: "none", paddingLeft: "0" }}>{e.address}</td>
                               <td style={{ borderBottom: "none", paddingLeft: "0" }}>{e.vout}</td>
