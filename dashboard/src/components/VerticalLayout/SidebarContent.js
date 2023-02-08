e690f9e261f00a006766602451ed3fe5b1482538
cluz123 <pmlizarraga@gmail.com>
2023-02-08 02:08:37 -0300

    staking button in sidebar added

--- dashboard/src/components/VerticalLayout/SidebarContent.js
+++ dashboard/src/components/VerticalLayout/SidebarContent.js
@@ -8,6 +8,8 @@ import SimpleBar from "simplebar-react"
 import MetisMenu from "metismenujs"
 import { withRouter } from "react-router-dom"
 import { Link } from "react-router-dom"
+import SweetAlert from "react-bootstrap-sweetalert"
+import { Card, CardBody, Col, Row, Button } from 'reactstrap';
 
 //i18n
 import { withTranslation } from "react-i18next"
@@ -87,8 +89,139 @@ const SidebarContent = props => {
     }
   }
 
+
+  
+  const [buttonStakingState, setButtonStakingState] = useState(true);
+
+  useEffect(()=> {
+    
+    fetch(`http://${window.location.hostname}:3001/getstakinginfo`, {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+    }).then(data => data.json())
+      .then(res => {
+        if(res.staking){
+          setButtonStakingState(true);
+        }else {
+          setButtonStakingState(false);
+        }
+      });
+  },[])
+
+
+  const [confirm_alert, setconfirm_alert] = useState(false)
+  const [confirm_alert2, setconfirm_alert2] = useState(false)
+  //const [success_msg, setsuccess_msg] = useState(false)
+  const [success_dlg, setsuccess_dlg] = useState(false)
+  const [dynamic_title, setdynamic_title] = useState("")
+  const [dynamic_description, setdynamic_description] = useState("")
+  const [error_dlg, seterror_dlg] = useState(false)
+
+
+
+  const [walletPassState, setWalletPassState] = useState(false);
+
+
+  function handleButton(stakingState) {
+    let titleRes;
+    let descriptionRes;
+    if (stakingState) {
+      fetch(`http://${window.location.hostname}:3001/walletlockforstaking`, {
+        method: 'GET',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        },
+      }).then(data => data.text())
+        .then(res => {
+          titleRes = "Staking Disabled"
+          descriptionRes = res;
+          setconfirm_alert2(false);
+          setsuccess_dlg(true);
+          setdynamic_title(titleRes);
+          setdynamic_description(descriptionRes);
+          setButtonStakingState(!buttonStakingState);
+        });
+    } else {
+
+      if (!walletPassState) {
+        titleRes = "Wallet password error!"
+        descriptionRes = "You must enter a password!"
+        setconfirm_alert2(false);
+        setdynamic_title(titleRes);
+        setdynamic_description(descriptionRes);
+        return seterror_dlg(true)
+      }
+
+
+      let objData = {
+        walletPassword: walletPassState
+      }
+      fetch(`http://${window.location.hostname}:3001/walletunlockforstaking`, {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(objData)
+      }).then(data => data.text())
+        .then(res => {
+          if ((res).includes("ok")) {
+            titleRes = "You are Staking!"
+            descriptionRes = "Staking enabled successfully";
+            setconfirm_alert2(false);
+            setsuccess_dlg(true);
+            setdynamic_title(titleRes);
+            setdynamic_description(descriptionRes);
+            setButtonStakingState(!buttonStakingState);
+          }else {
+            titleRes = "Wallet password error!"
+            descriptionRes = res;
+            setconfirm_alert2(false);
+            setdynamic_title(titleRes);
+            setdynamic_description(descriptionRes);
+            return seterror_dlg(true)
+          }
+        });
+    }
+
+  }
+
   return (
     <React.Fragment>
+      {success_dlg ? (
+        <SweetAlert
+          success
+          title={dynamic_title}
+          showConfirm={true}
+          timeout={0}
+          onConfirm={() => {
+            setsuccess_dlg(false)
+          }}
+        >
+          {dynamic_description}
+        </SweetAlert>
+      ) : null}
+
+      {error_dlg ? (
+        <SweetAlert
+          error
+          title={dynamic_title}
+          showConfirm={dynamic_title.includes("Wallet password error!") ? false : true}
+          timeout={dynamic_title.includes("Wallet password error!") ? 2 : 0}
+          onConfirm={() => {
+            setTimeout(() => {
+              seterror_dlg(false)
+              setconfirm_alert2(true);
+            }, 2000)
+          }}
+        >
+          {dynamic_description}
+        </SweetAlert>
+      ) : null}
       <SimpleBar style={{ maxHeight: "100%" }} ref={ref}>
         <div id="sidebar-menu">
           <ul className="metismenu list-unstyled" id="side-menu">
@@ -107,7 +240,7 @@ const SidebarContent = props => {
                 <span key="t-ui-elements">{props.t("Dashboard")}</span>
               </Link>
             </li>
-            
+
             <li>
               <Link to="/staking" className="waves-effect">
                 <i className='mdi mdi-pickaxe'></i>
@@ -122,15 +255,15 @@ const SidebarContent = props => {
               </Link>
             </li>
 
-               
+
             <li>
               <Link to="/blocks" className="waves-effect">
                 <i className='bx bxs-component'></i>
                 <span key="t-ui-elements">{props.t("Blocks")}</span>
               </Link>
             </li>
-            
-            
+
+
             <li>
               <Link to="/peers" className="waves-effect">
                 <i className='bx bx-radar'></i>
@@ -143,8 +276,49 @@ const SidebarContent = props => {
                 <i className='bx bx bxs-rocket'></i>
                 <span key="t-ui-elements">{props.t("Tips")}</span>
               </Link>
-            </li>           
-            
+            </li>
+            <li>
+              <Col xl={3} lg={4} sm={6} className="mb-2">
+                <div className="">
+                  <Button
+                    color={buttonStakingState ? "danger" : "primary"}
+                    onClick={() => {
+                      setconfirm_alert2(true)
+                    }}
+                    id="sa-success"
+                    className='m-2 mb-4'
+                  >
+                    <i className="mdi mdi-pickaxe"></i> {buttonStakingState ? "Disable" : "Enable"} Staking
+                  </Button>
+                </div>
+                {confirm_alert2 ? (
+                  <SweetAlert
+                    title={buttonStakingState ? "Staking will be disabled!" : "Staking will be enabled!"}
+                    warning
+                    showCancel
+                    confirmButtonText="Yes, do it!"
+                    confirmBtnBsStyle="success"
+                    cancelBtnBsStyle="danger"
+                    onConfirm={() => {
+                      { handleButton(buttonStakingState); }
+                      setconfirm_alert2(false);
+                    }}
+                    onCancel={() => setconfirm_alert2(false)}
+                  >
+                    <p>{buttonStakingState ? "You are going to disable mining on this node." : "Enter your wallet unlock password to enable staking."}</p>
+                    {!buttonStakingState && <input
+                      type="password"
+                      className="form-control"
+                      placeholder="Enter Wallet Password"
+                      onChange={(e) => {
+                        setWalletPassState(e.target.value);
+                      }}
+                    />}
+                  </SweetAlert>
+                ) : null}
+              </Col>
+            </li>
+
           </ul>
         </div>
       </SimpleBar>
