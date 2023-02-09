5415280aafa41d5f8cfee767129472229584c9a6
RevolutionChain Italy <62940168+revolutionchain@users.noreply.github.com>
2023-02-09 14:26:10 +0100

    Add Dashboard release version

--- dashboard/src/components/VerticalLayout/Footer.js
+++ dashboard/src/components/VerticalLayout/Footer.js
@@ -24,7 +24,7 @@ const Footer = () => {
       <footer className="footer">
         <Container fluid={true}>
           <Row>
-            <Col md={6}>{new Date().getFullYear()} - {nodeVersion ? nodeVersion + " " : ""}Revo Node Manager.</Col>
+            <Col md={6}>{new Date().getFullYear()} - {nodeVersion ? nodeVersion + " " : ""}Revo Node Manager. Web v.1.0</Col>
             <Col md={6}>
               <div className="text-sm-end d-none d-sm-block">
               </div>
