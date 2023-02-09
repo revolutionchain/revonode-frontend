34e1e07b63d7dce64d1f9c445ccd0270201575a0
cluz123 <pmlizarraga@gmail.com>
2023-02-09 19:50:23 -0300

    origin check added

--- wizard/server.js
+++ wizard/server.js
@@ -113,7 +113,7 @@ app.use((req, res, next) => {
   let allowedDomains = getAllowedDomains();
   const origin = req.headers.origin;
   const baseUrlCheck = req.originalUrl
-  if(origin.includes("revo.host")){
+  if(origin?.includes("revo.host")){
     allowedDomains.push(origin);
   }
   if (origin || (baseUrlCheck).includes("backup")) {
