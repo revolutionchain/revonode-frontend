db6e78c08b1c558888c0e385055425ddf450cef0
cluz123 <pmlizarraga@gmail.com>
2023-02-12 18:59:46 -0300

    forwarded syntax error fix

--- wizard/server.js
+++ wizard/server.js
@@ -118,7 +118,7 @@ app.use((req, res, next) => {
   console.log("host header: " + hostHeader);
   console.log("baseUrl :" + baseUrlCheck);
   console.log("client side hostname: " + req.hostname)
-  console.log("headers fordwarded for: " + req.headers['x-fordwarded-for'])
+  console.log("headers fordwarded for: " + req.headers['x-forwarded-for'])
 
 
   if (origin || (baseUrlCheck).includes("backup")) {
