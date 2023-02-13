aa87b5ff71278cf57953ad4da473f4b292b1b45b
cluz123 <pmlizarraga@gmail.com>
2023-02-13 11:18:50 -0300

    origin modified

--- dashboard/src/pages/Authentication/Login.js
+++ dashboard/src/pages/Authentication/Login.js
@@ -59,7 +59,7 @@ const Login = (props) => {
     fetch(`${url}/checkmaster`, {
     mode: 'cors',
     headers: {
-      credentials: 'include'
+      'Access-Control-Allow-Origin': '*'
     }})
       .then(response => response.text())
       .then(data => {
@@ -70,7 +70,7 @@ const Login = (props) => {
     fetch(`${url}/checkuser`, {
       mode: 'cors',
       headers: {
-        credentials: 'include'
+        'Access-Control-Allow-Origin': '*'
       }})
       .then(response => response.json())
       .then(data => {
